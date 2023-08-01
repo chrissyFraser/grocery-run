5cb607cfe8c116a48da85ec85bb4aca0f6fb2314
Chrissy Fraser <plum5152@gmail.com>
2023-07-31 21:59:43 -0700

    created superuser

---
+++ gr_accounts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GrAccountsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "gr_accounts"
---
+++ grocery_run/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GroceryRunConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "grocery_run"
