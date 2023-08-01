ceeba5db8ed9eb0863ba74fd2ee50474eaf1f491
Chrissy Fraser <plum5152@gmail.com>
2023-07-31 22:11:06 -0700

    feat: models

--- grocery_run/models.py
+++ grocery_run/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=100, null=True, default=1)
+    store_name = models.ForeignKey("Store", related_name="store", on_delete=models.PROTECT)
+    def __str__(self):
+        return self.name
+
+class Store(models.Model):
+    store_name = models.CharField(max_length=100, null=True, default="Any")
+
+    def __str__(self):
+        return self.name
