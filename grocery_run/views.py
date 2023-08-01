ac4be5f83b1b71af92f5f7d80db87271cda311d9
Chrissy Fraser <plum5152@gmail.com>
2023-07-31 22:22:52 -0700

    feat: item list view

--- grocery_run/views.py
+++ grocery_run/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from models import Item, Store
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import ListView
 
 # Create your views here.
+class ItemListView(LoginRequiredMixin, ListView):
+    model = Item
+    template_name = "grocery_run/item_list.html"
+
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+    
+    
