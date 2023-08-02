8ef9cd47bb506f975d4896b5c2976c23a74f5594
Chrissy Fraser <plum5152@gmail.com>
2023-08-01 18:26:26 -0700

    feat: login view & template

---
+++ gr_accounts/urls.py
@@ -0,0 +1,6 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+urlpatterns = [
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+]
--- gr_project/settings.py
+++ gr_project/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_REDIRECT_URL = "home"
 # Application definition
 
 INSTALLED_APPS = [
